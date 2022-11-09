555a0edf68136e649f30a1dc29f7bca37aedc9e3
knailkie <100939071+knailkie@users.noreply.github.com>
2022-11-09 14:11:29 +0100

    Add files via upload

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php 
+
+$db = new mysqli('localhost', 'root', '', 'debug_test' );
+
+$id = 1;
+$q = $db->prepare("SELECT * FROM test WHERE id = ?");
+$q->bind_param('i', $id);
+if($q->execute()) {
+    $result = $q->get_result();
+    $row = $result->fetch_assoc();
+} 
+
+?>
